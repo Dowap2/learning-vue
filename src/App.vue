2581ae9c65f72e6780c5e7c0a0899627e2622397
dowap2 <dowapdowari@gmail.com>
2021-04-14 20:35:15 +0900

    typing example code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="h" num="1" />
+    <HelloWorld msg="he" num="2" />
+    <HelloWorld msg="hel" num="3" />
+    <HelloWorld msg="hell" num="4" />
+    <HelloWorld msg="hello" num="5" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
