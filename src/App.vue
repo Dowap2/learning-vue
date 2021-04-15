82399205916e1bf5f5b8c2fab6fbac3bd58728b7
dowap2 <dowapdowari@gmail.com>
2021-04-15 19:53:28 +0900

    use v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    <HelloWorld msg="h" num="1" v-vind:seen="false" />
-    <HelloWorld msg="he" num="2" v-vind:seen="false" />
-    <HelloWorld msg="hel" num="3" v-vind:seen="false" />
-    <HelloWorld msg="hell" num="4" v-vind:seen="false" />
-    <HelloWorld msg="hello" num="5" v-vind:seen="true" />
+    <HelloWorld msg="h" num="1" v-bind:seen="true" />
+    <HelloWorld msg="he" num="2" v-bind:seen="false" />
+    <HelloWorld msg="hel" num="3" v-bind:seen="false" />
+    <HelloWorld msg="hell" num="4" v-bind:seen="false" />
+    <HelloWorld msg="hello" num="5" v-bind:seen="true" />
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }} {{ num }}</h1>
-    <p v-if="seen">원본 메시지{{ seen }}</p>
+    {{ seen }}
+    <p v-if="seen">원본 메시지</p>
   </div>
 </template>
 
