39cd473df13338bd8432eeb6830eb2fc2e4579ee
dowap2 <dowapdowari@gmail.com>
2021-04-17 16:56:57 +0900

    add component and data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,12 +5,17 @@
     <p v-if="seen">원본 메시지</p>
     <li v-for="(item, i) in list" v-bind:key="item">{{ item }} {{ i }}</li>
     {{ obj }}
+    <Post v-bind:post="{ id: 1, message: 'meesage' }" />
   </div>
 </template>
 
 <script>
+import Post from "./Post.vue";
 export default {
   name: "HelloWorld",
+  components: {
+    Post
+  },
   props: {
     msg: String,
     num: Number,
---
+++ src/components/Post.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="post">
+    {{ post.id }}
+    {{ post.message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    post: Object
+  }
+};
+</script>
